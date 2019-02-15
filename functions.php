1d06131f77e654eaf320c7a930ae1e10df9d3f85
intsamoilov <int.samoilov@gmail.com>
2019-02-15 20:30:12 +0300

    module5-task1 ver.1

--- functions.php
+++ functions.php
@@ -33,11 +33,69 @@ function includeTemplate($name, $data) {
 }
 
 /**
- * @param $date
- * @return false|int
+ * @param $config
+ * @return mysqli
  */
-function getSecondsForMidnight($date) {
-    $midnight = strtotime('tomorrow midnight');
-    $time_left = $midnight - $date;
-    return $time_left;
+function setDBConnection($config) {
+    $con = mysqli_init();
+    mysqli_options($con, MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+    mysqli_real_connect($con,
+        $config['host'],
+        $config['user'],
+        $config['password'],
+        $config['db_name']
+    );
+    mysqli_set_charset($con, "utf8");
+    if (!$con) {
+        print("Ошибка подключения: " . mysqli_connect_error());
+    }
+    return $con;
+}
+
+/**
+ * @param $date_end
+ * @return false|string
+ */
+function getRemainingTime($date_end) {
+    $current_time = strtotime('now');
+    $time_left = strtotime($date_end) - $current_time;
+    return gmdate('d H:i', $time_left);
+}
+
+/**
+ * @param $db
+ * @param $categories
+ * @return array|null
+ */
+function getCategoriesFromDB($db, $categories) {
+    $sql = 'select name'
+        . ' from categories'
+        . ' order by id';
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        print("Ошибка MySQL: " . mysqli_error($db));
+    }
+    return $categories;
+}
+
+/**
+ * @param $db
+ * @param $ads
+ * @return array|null
+ */
+function getAdsFromDB($db, $ads) {
+    $sql = 'select l.name as title, l.image as url, l.price, l.date_end, g.name as category'
+        . ' from lots l'
+        . ' left join categories g on l.category_id = g.id'
+        . ' where l.winner_id is null'
+        . ' order by l.date desc';
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        $ads = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        print("Ошибка MySQL: " . mysqli_error($db));
+    }
+    return $ads;
 }
--- templates/layout.php
+++ templates/layout.php
@@ -50,7 +50,7 @@
             <!--заполните этот список из массива категорий-->
             <?php foreach($categories as $value):?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?=htmlspecialchars($value);?></a>
+                <a href="pages/all-lots.html"><?=htmlspecialchars($value['name']);?></a>
             </li>
             <?php endforeach;?>
         </ul>
